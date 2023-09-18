f7ddedbb2fb81111074a2ee16e7828016997229f
ClintonYounge <clintonyounge18@gmail.com>
2023-09-18 17:39:57 -0400

    Update food controller
    - Updated the food controller create method
    - Adde private food params method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,10 +4,23 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
   end
 
   def new
     @food = Food.new
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
