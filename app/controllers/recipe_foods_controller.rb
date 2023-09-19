08047b0a86b3bf517d735d09ded552e1879974e3
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-20 02:24:40 +0300

    Add recipe-food methods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,7 +3,7 @@ class RecipeFoodsController < ApplicationController
 
   # GET /recipe_foods or /recipe_foods.json
   def index
-    @recipe_foods = RecipeFood.all
+    @recipe_foods = RecipeFood.where(recipe: @recipes)
   end
 
   # GET /recipe_foods/1 or /recipe_foods/1.json
@@ -11,7 +11,10 @@ class RecipeFoodsController < ApplicationController
 
   # GET /recipe_foods/new
   def new
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
+    @listed_foods = Food.where(user: current_user)
+
   end
 
   # GET /recipe_foods/1/edit
@@ -20,7 +23,7 @@ class RecipeFoodsController < ApplicationController
   # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
-
+    @recipe = @recipe_food.recipe
     respond_to do |format|
       if @recipe_food.save
         format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully created.' }
@@ -34,8 +37,9 @@ class RecipeFoodsController < ApplicationController
 
   # PATCH/PUT /recipe_foods/1 or /recipe_foods/1.json
   def update
+    @recipe_food = RecipeFood.find(params[:id])
     respond_to do |format|
-      if @recipe_food.update(recipe_food_params)
+      if @recipe_food.update(update_recipe_food_params)
         format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipe_food }
       else
@@ -60,6 +64,10 @@ class RecipeFoodsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_recipe_food
     @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update_recipe_food_params
+    params.require(:recipe_food).permit(:quantity)
   end
 
   # Only allow a list of trusted parameters through.
