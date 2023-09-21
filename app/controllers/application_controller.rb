81c33548d39f65848ad8c3d3e4dc4d1cd15fd0ee
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-21 21:38:04 +0300

    Implement cancan exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 
   protected
 
