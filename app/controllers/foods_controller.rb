d8ca3bd3281e65e3280039434cefa0939c2d2b0e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-21 22:13:17 +0300

    Fix linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,5 @@
 class FoodsController < ApplicationController
-
- load_and_authorize_resource
+  load_and_authorize_resource
   def index
     @foods = Food.all
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,10 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     user ||= User.new
 
-    if user.role == "admin"
+    if user.role == 'admin'
       can :manage, :all
     else
       can %i[read create], [Food, Recipe]
