7980b8d82598dae2780897fc77b92686bf6f606d
ClintonYounge <clintonyounge18@gmail.com>
2023-09-22 18:12:49 -0400

    Single functional shopping list item
    Made progress to get to functional code

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,7 +1,11 @@
 class ShoppingListController < ApplicationController
   def index
-    user_foods = Food.where(user_id: current_user.id)
-    recipe_foods = Food.joins(:recipes).where(recipes: { user_id: current_user.id })
-    @unused_foods = user_foods - recipe_foods
+    @user_foods = Food.where(user_id: current_user.id)
+
+    @recipe = Recipe.where(user_id: current_user.id).first
+    
+    @recipe_food = @recipe.recipe_foods
+
+    @all_recipe_foods = @recipe_food.includes([:food])
   end
 end
