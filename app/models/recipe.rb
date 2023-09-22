c7d599ce74dc147513f4ca9b11406b64a8bb1d13
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-22 19:20:35 +0300

    Add medel specs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 
-  validates :name, :public, presence: true
+  validates :name, presence: true
   validates :preparation_time, :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
