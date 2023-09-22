180ccac0fa35a1ba54452021b6b8ff4dac15412b
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-22 19:55:20 +0300

    Add recipe request tests

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -1,23 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
+RSpec.describe "Recipes", type: :request do 
+    let(:user) { User.create(name: 'test', email: 'test@gmail.com', password: 'password') }
     let(:recipe){Recipe.create(id: 1, name: "Test Recipe", public: true, preparation_time: 10, cooking_time: 20)}
-    describe "GET /recipes" do
-        it "works! (now write some real specs)" do
-            
 
+    before :each do
+      sign_in user
+    end
+
+    describe "GET /recipes" do
+        it "Returns successful response" do           
             get recipes_path
             expect(response).to have_http_status(:success)
-      expect(response).to render_template(:index)
-      expect(response.body).to include(recipe.name)
+            expect(response).to render_template(:index)
         end
     end
-    describe "GET /recipes/:id" do
-        it "works! (now write some real specs)" do
-            get recipe_path(recipe)
-            expect(response).to have_http_status(:success)
-      expect(response).to render_template(:show)
-      expect(response.body).to include(recipe.name)
-        end
-    end
+
 end
