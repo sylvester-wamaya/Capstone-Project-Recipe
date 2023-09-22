db1daac3ab0d1354a5c07a8bb8d369e45aebeb89
ClintonYounge <clintonyounge18@gmail.com>
2023-09-22 19:31:18 -0400

    Restructure shopping list controller and view

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,11 +1,17 @@
 class ShoppingListController < ApplicationController
   def index
-    @user_foods = Food.where(user_id: current_user.id)
+    user_foods = Food.where(user_id: current_user.id)
+    recipe_foods = RecipeFood.where(user_id: current_user.id)
 
-    @recipe = Recipe.where(user_id: current_user.id).first
-    
-    @recipe_food = @recipe.recipe_foods
+    @missing_foods = []
 
-    @all_recipe_foods = @recipe_food.includes([:food])
+    user_foods.each do |user_food|
+      recipe_food = recipe_foods.find_by(food_id: user_food.id)
+
+      if recipe_food.present? && recipe_food.quantity > user_food.quantity
+        missing_quantity = recipe_food.quantity - user_food.quantity
+        @missing_foods << { food: user_food, missing_quantity: missing_quantity }
+      end
+    end
   end
 end
