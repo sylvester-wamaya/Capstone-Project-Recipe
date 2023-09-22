40dfa267d592e8776051fe0bad001f51c2937681
ClintonYounge <clintonyounge18@gmail.com>
2023-09-22 19:32:06 -0400

    Update shopping list integration test

--- spec/integration/shopping_list_spec.rb
+++ spec/integration/shopping_list_spec.rb
@@ -7,30 +7,13 @@ RSpec.describe 'Shopping list page', type: :feature do
     sign_in user
   end
 
-  it 'should create a food, then show it in the shopping list' do
-    # create a food
-    visit foods_path
-    click_link 'New Food'
-    fill_in 'Name of food', with: 'test food'
-    fill_in 'Measurement unit', with: 'test unit'
-    fill_in 'Price', with: '1.0'
-    fill_in 'Quantity', with: '1'
-    click_button 'Create Food'
-
-    # visit the shopping list
-    visit shopping_list_index_path
-
-    # check and see if the food is added to the shopping list properly
-    expect(page).to have_content('test food')
-  end
-
   it "shouldn't show a food in the shopping list if it's in a recipe" do
     # create a food
     visit foods_path
     click_link 'New Food'
     fill_in 'Name of food', with: 'test food'
     fill_in 'Measurement unit', with: 'test unit'
-    fill_in 'Price', with: '1.0'
+    fill_in 'Price', with: '1'
     fill_in 'Quantity', with: '1'
     click_button 'Create Food'
 
@@ -47,13 +30,14 @@ RSpec.describe 'Shopping list page', type: :feature do
     visit recipes_path
     click_link 'test recipe'
     click_link 'Add ingredient'
-    fill_in 'Quantity', with: '1'
+    fill_in 'Quantity', with: '2'
     click_button 'Create Recipe food'
 
     # visit the shopping list
     visit shopping_list_index_path
 
     # check and see if the food is added to the shopping list properly
-    expect(page).to_not have_content('test food')
+    expect(page).to have_content('Amount of foods items to buy: 1')
+    expect(page).to have_content('Total value of food needed: $1')
   end
 end
